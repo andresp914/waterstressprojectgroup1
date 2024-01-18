f89313f3c664753abaac1d6dc5d4ba142af12a37
jrojcar98 <javirocar5@gmail.com>
2024-01-18 17:44:30 +0100

    .. new

---
+++ hl-rmd/ANOVAmodel.R
@@ -0,0 +1,12 @@
+an.ph <- aov(formula = variable ~ st.ph$Treatment)
+an.sh <- aov(formula = variable ~ st.sh$Treatment)
+an.ln <- aov(formula = variable ~ st.ln$Treatment)
+an.cc <- aov(formula = variable ~ st.cc$Treatment)
+an.afw <- aov(formula = variable ~ st.afw$Treatment)
+an.adw <- aov(formula = variable ~ st.adw$Treatment)
+an.rl <- aov(formula = variable ~ st.rl$Treatment)
+an.rfw <- aov(formula = variable ~ st.rfw$Treatment)
+an.rdw <- aov(formula = variable ~ st.rdw$Treatment)
+
+
+
---
+++ hl-rmd/boxplot.R
@@ -0,0 +1,13 @@
+datahl$variables <- datahl[[variables]]
+d <- datahl %>%
+  filter(!is.na(variables))
+
+b <- ggplot(d, aes(x = Species, y = variables, 
+              fill = Treatment)) +   
+  geom_boxplot()+
+  labs(x = "Species",
+       y = variablesname) +
+  theme_minimal() +
+  theme(plot.caption = element_text(hjust = 0), axis.text.x = element_text(face="italic", angle = 60, vjust = 1, hjust=1))+
+  scale_fill_brewer(palette="Set1") 
+
